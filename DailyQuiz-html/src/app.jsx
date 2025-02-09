5c2b181378007f1455c8068da7c7ba0a3796a2df
bhbakri <bakrihbassem@gmail.com>
2025-02-09 14:47:52 -0700

    Routes Setup

--- DailyQuiz-html/src/app.jsx
+++ DailyQuiz-html/src/app.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Quiz } from './quiz/quiz';
+import { Board } from './board/board';
+import { About } from './about/about';
 
 export default function App() {
-  return <div >
+  return <BrowserRouter><div >
     <body className='body'>
         <div className='headerPadding'>
     <header className='header'>
         <h1>Daily Quiz</h1>
         <nav>
             <menu>
-            <li><a className="navItemsOn" href="index.html">Home</a></li>
-              <li><a className="navItems" href="signup.html">Signup</a></li>
-              <li><a className="navItems" href="quiz.html">Quiz</a></li>
-              <li><a className="navItems" href="leaderboard.html">Leaderboard</a></li>
-              <li><a className="navItems" href="about.html">About</a></li>
+            <li><NavLink className="navItemsOn" to="">Home</NavLink></li>
+              <li><NavLink className="navItems" to="signup">Signup</NavLink></li>
+              <li><NavLink className="navItems" to="quiz">Quiz</NavLink></li>
+              <li><NavLink className="navItems" to="leaderboard">Leaderboard</NavLink></li>
+              <li><NavLink className="navItems" to="about">About</NavLink></li>
             </menu>
           </nav>
           <hr />
       </header>
       </div>
-      <main>hi!</main>
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path='/leaderboard' element={<Board />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
       <footer>
         <hr />
         <h2>Bassem Bakri</h2>
         <a href="https://github.com/bhbakri/startup">Website GitHub</a>
       </footer>
   </body>
-  </div>;
+  </div></BrowserRouter>;
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
